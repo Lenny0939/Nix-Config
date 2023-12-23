a8e2f4390e5cbaa63933d85cca157da9ac8ffb79
Lenny Escott <lennyescott@gmail.com>
2023-12-23 14:11:18 +1100

    fix themes.nix for pc

--- themes.nix
+++ themes.nix
@@ -3,15 +3,8 @@
   gtk.enable = true;
   gtk.cursorTheme.package = pkgs.bibata-cursors;
   gtk.cursorTheme.name = "Bibata-Modern-Ice";
-<<<<<<< HEAD
   #gtk.theme.package = pkgs.sweet;
   #gtk.theme.name = "sweet";
-  #gtk.iconTheme.package = pkgs.papirus-icon-theme;
-  #gtk.iconTheme.name = "Papirus";
-=======
-  gtk.theme.package = pkgs.sweet;
-  gtk.theme.name = "sweet";
   gtk.iconTheme.package = pkgs.papirus-icon-theme;
   gtk.iconTheme.name = "Papirus";
->>>>>>> pc
 }
