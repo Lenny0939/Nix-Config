43c1cf7dfeb5e38536dc05c5316ca9de6ffdcc8d
Lenny Escott <lennyescott@gmail.com>
2023-12-23 14:36:40 +1100

    change home.nix

--- home.nix
+++ home.nix
@@ -1,10 +1,9 @@
 { inputs, pkgs, lib, ... }: 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "lenny";
   home.homeDirectory = "/home/lenny";
   home.stateVersion = "24.05"; 
+  programs.home-manager.enable = true;
   imports = [
     inputs.hyprland.homeManagerModules.default
 		./wofi.nix
@@ -19,20 +18,4 @@
 		./waybar.nix
 		./spotify.nix
   ];
-  nixpkgs = {
-    config = {
-      allowUnfree = true;
-    };
-  };
-  programs.starship = {
-    enable = true;
-    #settings = pkgs.lib.importTOML ./starship.toml;
-  };
-	#programs.waybar = {
-	#	enable = true;
-	#	extraConfig = ''
-	#		
-	#	'';
-	#};
-  programs.home-manager.enable = true;
 }
