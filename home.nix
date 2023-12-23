6d4420356337bf832b49e95fb452866e6b025bc2
Lenny Escott <lennyescott@gmail.com>
2023-12-23 14:08:37 +1100

    fixed home.nix for pc

--- home.nix
+++ home.nix
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 { inputs, pkgs, ... }: 
 {
   home.username = "lenny";
@@ -9,49 +8,15 @@
     inputs.hyprland.homeManagerModules.default
 		./wofi.nix
 		./git.nix
-    ./packages.nix
     #./hyprland.nix
-=======
-{ inputs, pkgs, lib, ... }: 
-{
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = "lenny";
-  home.homeDirectory = "/home/lenny";
-  home.stateVersion = "24.05"; 
-  imports = [
-    inputs.hyprland.homeManagerModules.default
-		./wofi.nix
     ./packages.nix
-    ./hyprland.nix
 		./mangohud.nix
->>>>>>> pc
     ./zsh.nix
     ./themes.nix
     ./nvim
     ./firefox.nix
 		./kitty.nix
-<<<<<<< HEAD
 		./waybar.nix
-  ];
-=======
 		./spotify.nix
   ];
-  nixpkgs = {
-    config = {
-      allowUnfree = true;
-    };
-  };
-  programs.starship = {
-    enable = true;
-    #settings = pkgs.lib.importTOML ./starship.toml;
-  };
-	#programs.waybar = {
-	#	enable = true;
-	#	extraConfig = ''
-	#		
-	#	'';
-	#};
-  programs.home-manager.enable = true;
->>>>>>> pc
 }
