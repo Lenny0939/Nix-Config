60a8814e20c68ee04a84ed1fe2b062875bda0a9f
Lenny Escott <lennyescott@gmail.com>
2023-12-24 13:58:47 +1100

    updated wofi

--- wofi.nix
+++ wofi.nix
@@ -5,8 +5,12 @@
 		settings = {
 			insensitive= true;
 			allow_images = true;
+			mode = "drun";
 		};
 		style = ''
+			* {
+				border-radius: 5px;
+			}
 			window {
 				font-size: 28px;
 				font-family: "Hack Nerd Font";
@@ -15,7 +19,16 @@
 			}
 			#entry:selected {
 				background-color: #bbccdd;
+				background: linear-gradient(90deg, #bbccdd, #cca5dd);
 				color: #333333;
+			}
+			#input{
+				background-color:rgba(50,50,50,0.5);
+				color:white;
+				padding: 0.25em;
+			}
+			#entry{
+				padding: 0.25em;
 			}
 		'';
 	};
