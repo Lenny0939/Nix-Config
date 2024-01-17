598138ffbfe06f887be9fd664f695b681283e71f
Lenny Escott <lennyescott@gmail.com>
2024-01-18 06:22:59 +1100

    clarified ripgrep comment

--- modules/home/nvim/nixvim.nix
+++ modules/home/nvim/nixvim.nix
@@ -50,19 +50,15 @@
 						"<CR>" = "cmp.mapping.confirm({ select = true })";
 						"<Tab>" = {
 							action = ''
-								function(fallback)
-									if cmp.visible() then
-										cmp.select_next_item()
-									elseif luasnip.expandable() then
-										luasnip.expand()
-									elseif luasnip.expand_or_jumpable() then
-										luasnip.expand_or_jump()
-									elseif check_backspace() then
-										fallback()
-									else
-										fallback()
-									end
-								end
+					function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_locally_jumpable() then
+                luasnip.expand_or_jump()
+            else
+                fallback()
+            end
+						end
 							'';
 							modes = [ "i" "s" ];
 						};
