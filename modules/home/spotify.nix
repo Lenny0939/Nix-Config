b4b29fb658f3846aa27e807f8948e4d15ce29850
Lenny Escott <lennyescott@gmail.com>
2024-02-11 17:57:42 +1100

    hid password in spotify

--- modules/home/spotify.nix
+++ modules/home/spotify.nix
@@ -1,11 +1,11 @@
-{ config, pkgs, ... }:
+{ ... }:
 {
 	services.spotifyd = {
 		enable = true;
 		settings = {
 			global = {
 				username = "8e1gctsj9m4vyqsupbimkn1xy";
-				password = "***REMOVED***";
+				password = import ./spotifypassword;
 			};
 		};
 	};
