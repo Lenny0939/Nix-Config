2f1e00c69318f4c18a9d9337d6cdffff58f08eb3
Lenny Escott <lennyescott@gmail.com>
2024-02-24 06:22:23 +1100

    made scaling 1.6 because hyprland wants it to for some reason

--- aragorn/aragorn.nix
+++ aragorn/aragorn.nix
@@ -5,8 +5,7 @@
 		./hardware-configuration-aragorn.nix
 		../modules/fonts.nix
 	];
-	#home-manager.users.lenny.
-	home-manager.users.lenny.wayland.windowManager.hyprland.settings.monitor = ",preferred,auto,1.5";
+	home-manager.users.lenny.wayland.windowManager.hyprland.settings.monitor = ",preferred,auto,1.6";
 	networking.hostName = "aragorn"; 
 	environment.sessionVariables = { WLR_NO_HARDWARE_CURSORS = "1"; };
 	hardware.opengl = {
