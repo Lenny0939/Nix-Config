b32110686ee32b9c67624063b6828a33a40fa1c0
Lenny0939 <lennyescott@gmail.com>
2024-02-26 16:08:56 +1100

    added zsh vi mode and changed from fast-syntax-highlighting to zsh-syntax-highlighting

--- home/zsh.nix
+++ home/zsh.nix
@@ -24,30 +24,28 @@
       };
     }
     {
-      name = "fast-syntax-highlighting";
-      file = "fast-syntax-highlighting.plugin.zsh";
+      name = "syntax-highlighting";
+			file = "zsh-syntax-highlighting.plugin.zsh";
       src = pkgs.fetchFromGitHub {
-        owner = "zdharma";
-        repo = "fast-syntax-highlighting";
-        rev = "5fab542516579bdea5cc8b94137d9d85a0c3fda5";
-        sha256 = "1ff1z2snbl9rx3mrcjbamlvc21fh9l32zi2hh9vcgcwbjwn5kikg";
+        owner = "zsh-users";
+        repo = "zsh-syntax-highlighting";
+        rev = "master";
+        sha256 = "sha256-4rW2N+ankAH4sA6Sa5mr9IKsdAg7WTgrmyqJ2V1vygQ=";
       };
     }
-		/*{
-			name = "zsh-vi-mode";
-			src = pkgs.fetchFromGitHub {
-				owner = "jeffreytse";
-				repo = "zsh-vi-mode";
-				rev = "v0.11.0";
-				sha256 = "sha256-xbchXJTFWeABTwq6h4KWLh+EvydDrDzcY9AQVK65RS8=";
-			};
-		}*/
+    {
+      name = "zsh-vi-mode";
+      src = pkgs.fetchFromGitHub {
+        owner = "jeffreytse";
+        repo = "zsh-vi-mode";
+        rev = "v0.11.0";
+        sha256 = "sha256-xbchXJTFWeABTwq6h4KWLh+EvydDrDzcY9AQVK65RS8=";
+      };
+    }
     ];
-		/*initExtra = ''
-			source ${pkgs.zsh-vi-mode}/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh
+		initExtra = ''
+			fastfetch
 		'';
-		*/
-		initExtra = "fastfetch";
   };  
 	programs.zoxide.enable = true;
 	programs.starship = {
