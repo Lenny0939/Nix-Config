450ce8ac5d28fd3f32f6570e70324f7f65f3c458
Lenny0939 <lennyescott@gmail.com>
2024-03-06 08:57:21 +1100

    addded timeout, height and font to mako

--- home/mako.nix
+++ home/mako.nix
@@ -2,11 +2,14 @@
 {
 	services.mako = with config.colorScheme.palette; {
     enable = true;
+		font = "${config.font}";
     backgroundColor = "#${base00}";
     borderColor = "#${base0E}";
     borderRadius = 5;
     borderSize = 2;
     textColor = "#${base05}";
     layer = "overlay";
+		height = 200;
+		defaultTimeout = 5000;
   };
 }
