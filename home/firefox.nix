1504fc9432f83c8fb08d85550c8404712cf4a258
Lenny0939 <lennyescott@gmail.com>
2024-03-18 18:57:34 +1100

    added firefox nightly

--- home/firefox.nix
+++ home/firefox.nix
@@ -1,7 +1,17 @@
 { pkgs, config, ... }:
 {
+	nixpkgs.overlays =
+  let
+    # Change this to a rev sha to pin
+    moz-rev = "master";
+    moz-url = builtins.fetchTarball { url = "https://github.com/mozilla/nixpkgs-mozilla/archive/${moz-rev}.tar.gz"; sha256 = "sha256:1f41psqw00mdcwm28y1frjhssybg6r8i7rpa8jq0jiannksbj27s";};
+    nightlyOverlay = (import "${moz-url}/firefox-overlay.nix");
+  in [
+    nightlyOverlay
+  ];
 	programs.firefox = {
 		enable = true;
+		package = pkgs.latest.firefox-nightly-bin;
 		profiles.lenchog = {
 			search = {
 				engines = {
