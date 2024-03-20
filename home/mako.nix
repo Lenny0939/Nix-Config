84a9d5db4c6a40e2293ebaea143d06d77d215696
Lenny0939 <lennyescott@gmail.com>
2024-03-20 20:04:15 +1100

    removed a some mako config as it is now managed by stylix

--- home/mako.nix
+++ home/mako.nix
@@ -2,12 +2,12 @@
 {
 	services.mako = with config.colorScheme.palette; {
     enable = true;
-		font = "${config.font}";
-    backgroundColor = "#${base00}";
-    borderColor = "#${base0E}";
+		#font = "${config.font}";
+    # backgroundColor = "#${base00}";
+    # borderColor = "#${base0E}";
     borderRadius = 5;
     borderSize = 2;
-    textColor = "#${base05}";
+    # textColor = "#${base05}";
     layer = "overlay";
 		height = 200;
 		defaultTimeout = 5000;
