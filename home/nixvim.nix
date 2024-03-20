151e3f78f52c56ea341f1780a389ae17f116f87d
Lenny0939 <lennyescott@gmail.com>
2024-03-20 15:04:58 +1100

    fixed things in nixvim and removed theme, as it is now managed by stylix

--- home/nixvim.nix
+++ home/nixvim.nix
@@ -2,10 +2,6 @@
 {
   programs.nixvim = {
     enable = true;
-		colorschemes.catppuccin = {
-		  enable = true;
-			transparentBackground = true;
-		};
 		globals.mapleader = " ";
 		options = {
 			linebreak = true;
@@ -42,25 +38,33 @@
 				};
 				cmp = {
 					enable = true;
-					autoEnableSources = true;
 					settings = { 
-						sources = [
-							{name = "nvim_lsp";}
-							{name = "luasnip";}
-							{name = "path";}
-							{name = "buffer";}
-						];
 						mapping = {
-						__raw = /* lua */ ''
+							__raw = /* lua */ ''
 								cmp.mapping.preset.insert({
 									['<C-b>'] = cmp.mapping.scroll_docs(-4),
 									['<C-f>'] = cmp.mapping.scroll_docs(4),
 									['<C-Space>'] = cmp.mapping.complete(),
 									['<C-e>'] = cmp.mapping.abort(),
 									['<CR>'] = cmp.mapping.confirm({ select = true }),
-									['Tab'] = cmp.select_next_item(),
+									['Tab'] = cmp.mapping.select_next_item(),
 								})
-						'';
+							'';
+						};
+						snippet = {
+							expand = "luasnip";
+						};
+						sources = {
+							__raw = /* lua */ ''
+								cmp.config.sources({
+									{ name = 'nvim_lsp' },
+									{ name = 'vsnip' },
+									{ name = 'luasnip' },
+									{ name = 'path' },
+								}, {
+									{ name = 'buffer' },
+								})
+							'';
 						};
 					};
 				};
