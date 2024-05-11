d3f590d1c5c8c41a96611a99b6ca27e8b7e06d5d
Lenny Escott <lennyescott@gmail.com>
2024-05-12 08:32:15 +1000

    got rid of pkgs.zoxide thingo because z bin is weird

--- home/zsh.nix
+++ home/zsh.nix
@@ -7,7 +7,7 @@
 			wifi = ''${pkgs.networkmanager}/bin/nmcli --ask dev wifi list | ${pkgs.coreutils}/bin/tail -n +2 | ${pkgs.gnused}/bin/sed 's/\*//g' | ${pkgs.gawk}/bin/awk '{ print $2" "$5" "$6" "$7 "%" }' | ${pkgs.fzf}/bin/fzf --bind 'enter:become(${pkgs.networkmanager}/bin/nmcli --ask dev wifi connect)' | ${pkgs.gawk}/bin/awk '{ print $1 }' '';
 			ff = "${pkgs.fzf}/bin/fzf --preview '${pkgs.pistol}/bin/pistol {}' --bind 'enter:become($EDITOR {})'";
 			gyat = "${pkgs.git}/bin/git";
-			cd = "${pkgs.zoxide}/bin/z";
+			cd = "z";
 			ls = "${pkgs.lsd}/bin/lsd";
 			nix = "${pkgs.nix-output-monitor}/bin/nom";
 			nix-build = "${pkgs.nix-output-monitor}/bin/nom-build";
