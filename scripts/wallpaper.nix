71a0dfb77fdc039d4851c302e4072fc8859c48e1
Lenny Escott <lennyescott@gmail.com>
2024-07-01 16:07:48 +1000

    added wallpaper script

---
+++ scripts/wallpaper.nix
@@ -0,0 +1,11 @@
+{ pkgs, ... }:
+{
+	home.packages = with pkgs; [
+		(writeShellScriptBin "wallpaper" /* bash */ ''
+			MONITOR=$(hyprctl monitors -j | ${jq}/bin/jq -r '.[] | .name')
+			WALLPAPER=$(${fd}/bin/fd . ${../wallpapers} | ${fzf}/bin/fzf --delimiter / --with-nth -1 --preview='${kitty}/bin/kitten icat --clear --transfer-mode=memory --stdin=no --place="$(($COLUMNS/2))"x"$(($LINES))"@"$(($COLUMNS/2))"x0 --align center {} > /dev/tty')
+			hyprctl hyprpaper preload "$WALLPAPER"
+			hyprctl hyprpaper wallpaper "$MONITOR,$WALLPAPER"
+		'')
+	];
+}
