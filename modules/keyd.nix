c603c981fb49ae01644fbed2b12291060401173a
Lenny0939 <lennyescott@gmail.com>
2024-08-28 14:57:53 +1000

    added recurva

--- modules/keyd.nix
+++ modules/keyd.nix
@@ -2,8 +2,13 @@
 let
 	canary = [
 		"w" "l" "y" "p" "k" "z" "x" "o" "u" ";"
-		 "c" "r" "s" "t" "b" "f" "n" "e" "i" "a"
+		 "c" "r" "s" "t" "b" "f" "n" "e" "i" "a" "'"
 		  "j" "v" "d" "g" "q" "m" "h" "/" "," "."
+	];
+	recurva = [
+		"f" "r" "d" "p" "v" "q" "j" "u" "o" "y"
+		 "s" "n" "t" "c" "b" "." "h" "e" "a" "i" "/"
+		   "x" "k" "g" "w" "z" "m" "l" ";" "'" ","
 	];
 in
 {
@@ -14,8 +19,45 @@ in
 				main = {
 					rightalt = "layer(raise)";
 					leftalt = "oneshot(alt)";
-					rightcontrol = "toggle(qwerty)";
+					rightcontrol = "toggle(canary)";
+					#capslock = "C-backspace";
 					capslock = "backspace";
+				};
+				recurva = {
+					q = builtins.elemAt recurva 0;
+					w = builtins.elemAt recurva 1;
+					e = builtins.elemAt recurva 2;
+					r = builtins.elemAt recurva 3;
+					t = builtins.elemAt recurva 4;
+					y = builtins.elemAt recurva 5;
+					u = builtins.elemAt recurva 6;
+					i = builtins.elemAt recurva 7;
+					o = builtins.elemAt recurva 8;
+					p = builtins.elemAt recurva 9;
+					a = builtins.elemAt recurva 10;
+					s = builtins.elemAt recurva 11;
+					d = builtins.elemAt recurva 12;
+					f = builtins.elemAt recurva 13;
+					g = builtins.elemAt recurva 14;
+					h = builtins.elemAt recurva 15;
+					j = builtins.elemAt recurva 16;
+					k = builtins.elemAt recurva 17;
+					l = builtins.elemAt recurva 18; 
+					";" = builtins.elemAt recurva 19;
+					"'" = builtins.elemAt recurva 20;
+					z = builtins.elemAt recurva 21;
+					x = builtins.elemAt recurva 22;
+					c = builtins.elemAt recurva 23;
+					v = builtins.elemAt recurva 24;
+					b = builtins.elemAt recurva 25;
+					n = builtins.elemAt recurva 26;
+					m = builtins.elemAt recurva 27;
+					"," = builtins.elemAt recurva 28;
+					"." = builtins.elemAt recurva 29;
+					"/" = builtins.elemAt recurva 30;
+					rightcontrol = "toggle(canary)";
+				};
+				canary = {
 					q = builtins.elemAt canary 0;
 					w = builtins.elemAt canary 1;
 					e = builtins.elemAt canary 2;
@@ -36,18 +78,20 @@ in
 					k = builtins.elemAt canary 17;
 					l = builtins.elemAt canary 18; 
 					";" = builtins.elemAt canary 19;
-					z = builtins.elemAt canary 20;
-					x = builtins.elemAt canary 21;
-					c = builtins.elemAt canary 22;
-					v = builtins.elemAt canary 23;
-					b = builtins.elemAt canary 24;
-					n = builtins.elemAt canary 25;
-					m = builtins.elemAt canary 26;
-					"," = builtins.elemAt canary 27;
-					"." = builtins.elemAt canary 28;
-					"/" = builtins.elemAt canary 29;
+					"'" = builtins.elemAt canary 20;
+					z = builtins.elemAt canary 21;
+					x = builtins.elemAt canary 22;
+					c = builtins.elemAt canary 23;
+					v = builtins.elemAt canary 24;
+					b = builtins.elemAt canary 25;
+					n = builtins.elemAt canary 26;
+					m = builtins.elemAt canary 27;
+					"," = builtins.elemAt canary 28;
+					"." = builtins.elemAt canary 29;
+					"/" = builtins.elemAt canary 30;
+					rightcontrol = "toggle(recurva)";
 				};
-				"qwerty" = {
+				qwerty = {
 					q = "q";
 					w = "w";
 					e = "e";
