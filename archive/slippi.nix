8ce60f73cbbd6f42a49bc7b51a75fbf08f35c9e3
Lenny0939 <lennyescott@gmail.com>
2024-09-22 13:58:59 +1000

    got rid of slippi and spotify

---
+++ archive/slippi.nix
@@ -0,0 +1,8 @@
+{ ... }:
+{
+	slippi-launcher = {
+			isoPath = "/users/lenny/home/Projects/Retro Games/melee/animelee.iso";
+			launchMeleeOnPlay = false;
+			enableJukebox = true;
+	};
+}
--- home/slippi.nix
+++
@@ -1,8 +0,0 @@
-{ ... }:
-{
-	slippi-launcher = {
-			isoPath = "/users/lenny/home/Projects/Retro Games/melee/normal-melee.iso";
-			launchMeleeOnPlay = true;
-			enableJukebox = true;
-	};
-}
